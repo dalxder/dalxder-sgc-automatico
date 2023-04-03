b3e944ff0c3ae13d12a5fce72d1c0fe6d31a16c3
dalxder <dalxder@gmail.com>
2023-04-03 07:57:59 -0500

    1 file modified, 1 added

--- src/main.ts
+++ src/main.ts
@@ -7,22 +7,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
   selector: 'my-app',
   standalone: true,
   imports: [CommonModule],
-  template: `
-  <h1>Prototipo de Aplicación para gestión de la calidad </h1>
-  <ul>
-    <li>
-        Control de calidad en el ingreso-datos a la base de datos catastral
-    </li>
-    <li>
-      Aplicación Cliente de base de datos desarrollada en Python 
-      </li>
-    <li>      
-    Controlador de conección a base de datos (PosgreSQL)
-
-    </li>
-  </ul>
-
-  `,
+  template: '.main.component.html',
 })
 export class App {
   name = 'Angular';
